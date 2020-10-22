16b6932472552a5da1c4b2e629383ccad6deb0d8
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2020-10-22 15:50:51 -0400

    Updates

---
+++ precache-manifest.9dff8bded7f79d14dfec1aae5ec2dcb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fe4a0a84ca4682dc56b4aea69cbedf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b2a1c6e2a87b2ec318",
+    "url": "/static/css/main.11ed65c8.chunk.css"
+  },
+  {
+    "revision": "4bfce82a405f06b41ab3",
+    "url": "/static/js/2.e723929e.chunk.js"
+  },
+  {
+    "revision": "b98d1f83664e63c5fb6f37ed7507de6f",
+    "url": "/static/js/2.e723929e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b2a1c6e2a87b2ec318",
+    "url": "/static/js/main.8028af51.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);

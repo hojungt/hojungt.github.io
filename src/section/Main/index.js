e5177a22a6025fc650f0591facfac621716c5373
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2020-11-19 16:44:03 -0500

    fix container and row alignment

--- src/section/Main/index.js
+++ src/section/Main/index.js
@@ -5,8 +5,8 @@ import image from "../../images/rachael-tseng.jpg";
 export default function Main() {
     return (
         <div className="img-bg-main">
-            <div className="container" id="main">
-                <div className="row h-100 justify-content-center">
+            <div className="container h-100" id="main">
+                <div className="row h-100 justify-content-center align-items-center">
                     <div className="col-12 col-lg-3">
                         <h1>Hi, I'm Rachael Tseng.</h1>
                     </div>

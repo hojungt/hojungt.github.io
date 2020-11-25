bb5d470f4655a930c3cdefb5ddf468ec6cbf3919
Ho-Jung Tseng (Rachael) <45810198+hojungt@users.noreply.github.com>
2020-11-24 21:07:15 -0500

    Updates

---
+++ precache-manifest.12fd96ea96a0e0d7782fea41cf9d1f49.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85830e9b78bb112152e516e02d40de4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d632b90ae4230b9ae318",
+    "url": "/static/css/main.bd3b9965.chunk.css"
+  },
+  {
+    "revision": "9fccc60f5b72fd7de032",
+    "url": "/static/js/2.5099eda4.chunk.js"
+  },
+  {
+    "revision": "36b3a2123f180e082ee45d579f114228",
+    "url": "/static/js/2.5099eda4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d632b90ae4230b9ae318",
+    "url": "/static/js/main.177552fa.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "0be33336b3f7c45b42e2de4783afdabb",
+    "url": "/static/media/bench-accounting-nvzvOPQW0gc-unsplash.0be33336.jpg"
+  },
+  {
+    "revision": "6597a09c328e205b27e8fe7242ee0fda",
+    "url": "/static/media/bench-accounting-xxeAftHHq6E-unsplash.6597a09c.jpg"
+  },
+  {
+    "revision": "5cec7648811ef81f5725b7cc9c4024bd",
+    "url": "/static/media/elena-joland-mjeQon0Mh_Q-unsplash.5cec7648.jpg"
+  },
+  {
+    "revision": "583689282a70830fcdc405df2caae548",
+    "url": "/static/media/logo_Groundcycle.58368928.png"
+  },
+  {
+    "revision": "859b17580283455d4c3a2dcef47cbd33",
+    "url": "/static/media/other_drawing.859b1758.jpg"
+  },
+  {
+    "revision": "1b816b1e716c19830a67d156e0a100b3",
+    "url": "/static/media/other_photography.1b816b1e.jpg"
+  },
+  {
+    "revision": "5d40e5210f78982fe8d3f5c1fc6dc188",
+    "url": "/static/media/other_thesis.5d40e521.jpg"
+  },
+  {
+    "revision": "939747901b1d60fd55f46b1b3355e4d7",
+    "url": "/static/media/philipp-berndt-5i0GnoTTjSE-unsplash.93974790.jpg"
+  },
+  {
+    "revision": "e8022cc74c0844045af301624cd43523",
+    "url": "/static/media/project_125greenwich.e8022cc7.jpg"
+  },
+  {
+    "revision": "00d2b2bf81c67e153bda995b1963debf",
+    "url": "/static/media/project_531aoa.00d2b2bf.jpg"
+  },
+  {
+    "revision": "5e85bebc4eb4f8b255ee93385d5fed45",
+    "url": "/static/media/project_groundcycle.5e85bebc.jpg"
+  },
+  {
+    "revision": "a7e0ffb6cedee92ad8e0de2e0a9fecbd",
+    "url": "/static/media/project_questgiver.a7e0ffb6.png"
+  },
+  {
+    "revision": "53c3b9dc2015b7e03ae917d41beea680",
+    "url": "/static/media/project_something-borrowed.53c3b9dc.jpg"
+  },
+  {
+    "revision": "c3dedde2461f144bef3c9047e886a093",
+    "url": "/static/media/project_spafinder.c3dedde2.jpg"
+  },
+  {
+    "revision": "bce97db2e3601b4a148bdb70f56925fb",
+    "url": "/static/media/rachael-tseng.bce97db2.jpg"
+  },
+  {
+    "revision": "e78a14903250a4682e3e0fe2d105e5c9",
+    "url": "/static/media/samantha-gades-BlIhVfXbi9s-unsplash.e78a1490.jpg"
+  },
+  {
+    "revision": "7ddff99ff5da3ca646b5a1df8f38c73b",
+    "url": "/static/media/scott-webb-zblX5hM9rp8-unsplash.7ddff99f.jpg"
+  },
+  {
+    "revision": "dd4b3dcf0eb62c0cde2c44eebbc403dc",
+    "url": "/static/media/wizard.dd4b3dcf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4b467cb89fd8a3e0cc928b32be6e8a9.js"
+  "/precache-manifest.12fd96ea96a0e0d7782fea41cf9d1f49.js"
 );
 
 self.addEventListener('message', (event) => {
